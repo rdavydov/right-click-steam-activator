3275d1d0e7a41e2923e8f6c06f587b633482a77e
Roman Davydov <15850461+rdavydov@users.noreply.github.com>
2023-03-18 22:37:40 +0300

    2.0

---
+++ options.js
@@ -0,0 +1,58 @@
+const optionsForm = document.getElementById('options-form');
+const sessionidInput = document.getElementById('sessionid-input');
+const steamLoginSecureInput = document.getElementById('steamLoginSecure-input');
+const infoBox = document.getElementById('info-box');
+
+optionsForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const sessionid = sessionidInput.value;
+    const steamLoginSecure = steamLoginSecureInput.value;
+
+    if (sessionid && steamLoginSecure) {
+        chrome.storage.sync.set({ sessionid, steamLoginSecure }, () => {
+            if (sessionid) {
+                chrome.cookies.set({
+                    url: "https://store.steampowered.com/",
+                    name: "sessionid",
+                    value: sessionid,
+                    secure: true,
+                    httpOnly: true
+                }, () => {
+                    console.log("sessionid cookie value is set");
+                });
+            }
+
+            if (steamLoginSecure) {
+                chrome.cookies.set({
+                    url: "https://store.steampowered.com/",
+                    name: "steamLoginSecure",
+                    value: steamLoginSecure,
+                    secure: true,
+                    httpOnly: true
+                }, () => {
+                    console.log("steamLoginSecure cookie value is set");
+                });
+            }
+
+            // Update existing info box
+            infoBox.classList.remove('error');
+            infoBox.classList.add('success');
+            infoBox.textContent = 'Cookie values are set for the extension and for the Steam website.';
+        });
+    } else {
+        chrome.storage.sync.set({ sessionid: "", steamLoginSecure: "" }, () => {
+            sessionidInput.value = '';
+            steamLoginSecureInput.value = '';
+            // Update existing info box
+            infoBox.classList.remove('success');
+            infoBox.classList.add('error');
+            infoBox.textContent = 'Cookie values are cleared for the extension. We\'ll get them from the Steam website.';
+        });
+    }
+});
+
+chrome.storage.sync.get(['sessionid', 'steamLoginSecure'], ({ sessionid, steamLoginSecure }) => {
+    sessionidInput.value = sessionid || '';
+    steamLoginSecureInput.value = steamLoginSecure || '';
+});
--- right-click-steam-activator.js
+++ right-click-steam-activator.js
@@ -1,70 +1,121 @@
 function activatekey(info) {
-    const _re = new RegExp('^[0-9A-Z]{4,7}-[0-9A-Z]{4,7}-[0-9A-Z]{4,7}(?:(?:-[0-9A-Z]{4,7})?(?:-[0-9A-Z]{4,7}))?$', 'i');
-    const _key = info.selectionText.trim();
-    const _valid = (_key != '') && _re.test(_key);
+    // const _re = new RegExp('^[0-9A-Z]{4,7}-[0-9A-Z]{4,7}-[0-9A-Z]{4,7}(?:(?:-[0-9A-Z]{4,7})?(?:-[0-9A-Z]{4,7}))?$', 'i');
 
-    if (_valid) {
+    // Declare a regular expression that matches a string of 4-5 alphanumeric characters, 
+    // followed by a hyphen and another string of 4-5 alphanumeric characters, repeated 
+    // 2-4 times. This pattern is case-insensitive.
+    const regex = /[0-9A-Z]{4,5}(?:-[0-9A-Z]{4,5}){2,4}/i;
+    // Trim leading and trailing whitespace from the selected text and attempt to 
+    // match it against the regular expression.
+    // ?. is an optional chaining operator to safely access the first element of the resulting array.
+    // If there is no match, it will return undefined instead of throwing an error.
+    const _key = info.selectionText.trim().match(regex)?.[0];
+
+    if (_key) {
         console.log("Activating Steam key: ", _key);
-        // Retrieve sessionid cookie
-        chrome.cookies.get({ url: "https://store.steampowered.com/", name: "sessionid" }, function (cookie) {
-            if (cookie) {
-                // Create form data
-                const formData = new FormData();
-                formData.append('product_key', _key);
-                formData.append('sessionid', cookie.value);
 
-                // Send POST request to activate key
-                fetch('https://store.steampowered.com/account/ajaxregisterkey/', {
-                    method: 'POST',
-                    credentials: 'include',
-                    body: formData
+        // Retrieve sessionid from storage or default to empty string using promises
+        const sessionidPromise = new Promise((resolve, reject) => {
+            chrome.storage.sync.get({ sessionid: "" }, function ({ sessionid }) {
+                if (sessionid) {
+                    console.log("Using user-supplied sessionid cookie:", sessionid);
+                    resolve(sessionid);
+                } else {
+                    console.log("User-supplied sessionid cookie not found in storage. Getting it from the Steam website.");
+                    // Retrieve sessionid cookie from https://store.steampowered.com/
+                    chrome.cookies.get(
+                        { url: "https://store.steampowered.com/", name: "sessionid" },
+                        function (cookie) {
+                            if (cookie) {
+                                const sessionid = cookie.value;
+                                console.log("Retrieved sessionid cookie from https://store.steampowered.com/:", sessionid);
+                                resolve(sessionid);
+                            } else {
+                                const error = new Error("sessionid cookie not found!");
+                                alert(error);
+                                reject(error);
+                            }
+                        }
+                    );
+                }
+            });
+        });
+
+        // Use the sessionid value after it has been retrieved from storage or cookies
+        sessionidPromise.then((sessionid) => {
+            // Create form data
+            const formData = new FormData();
+            formData.append('product_key', _key);
+            formData.append('sessionid', sessionid);
+
+            // Send POST request to activate key
+            fetch('https://store.steampowered.com/account/ajaxregisterkey/', {
+                method: 'POST',
+                credentials: 'include',
+                body: formData
+            })
+                .then(response => response.json())
+                .catch(error => {
+                    alert("Failed to parse response as JSON. Most likely you are not logged in to Steam in the browser.");
+                    console.error("Failed to parse response as JSON: " + error);
+                    throw error;
                 })
-                    .then(response => response.json())
-                    .then(data => {
-                        if (data.success === 1) {
+                .then(data => {
+                    switch (data.success) {
+                        case 1:
                             alert("Activated: " + data.purchase_receipt_info.line_items[0].line_item_description);
                             return;
-                        }
+                        case 21:
+                            alert("ERROR! Not a valid sessionid: " + sessionid);
+                            return;
+                        case 2:
+                            console.log("'success' is 2. Key is not activated.")
+                            break;
+                        default:
+                            alert("success: " + data.success);
+                            break;
+                    }
 
-                        // const resultDetail = data.purchase_receipt_info.result_detail;
+                    // const resultDetail = data.purchase_receipt_info.result_detail;
 
-                        switch (data.purchase_result_details) {
-                            case 53:
-                                alert("Temporary ban from Steam. Ban should lift in 45-60 minutes.");
-                                return;
-                            case 9:
-                                alert("Already own: " + data.purchase_receipt_info.line_items[0].line_item_description);
-                                return;
-                            case 14:
-                                alert("Invalid key: " + _key);
-                                return;
-                            case 15:
-                                alert("Somebody already activated this code for: " + data.purchase_receipt_info.line_items[0].line_item_description);
-                                return;
-                            case 13:
-                                alert("Regional restrictions: " + data.purchase_receipt_info.line_items[0].line_item_description);
-                                return;
-                            case 24:
-                                alert("Missing base game: " + data.purchase_receipt_info.line_items[0].line_item_description);
-                                return;
-                            case 36:
-                                alert("Need a PS3?");
-                                return;
-                            case 50:
-                                alert("This is the recharge code!");
-                                return;
-                            default:
-                                alert("purchase_result_details: " + data.purchase_result_details);
-                                break;
-                        }
-                    })
-                    .catch(error => console.error(error));
-            } else {
-                alert("ERROR! sessionid cookie not found!");
-            }
+                    switch (data.purchase_result_details) {
+                        case 53:
+                            alert("Temporary ban from Steam. Ban should lift in 45-60 minutes.");
+                            return;
+                        case 9:
+                            alert("Already own: " + data.purchase_receipt_info.line_items[0].line_item_description);
+                            return;
+                        case 14:
+                            alert("Invalid key: " + _key);
+                            return;
+                        case 15:
+                            alert("Somebody already activated this code for: " + data.purchase_receipt_info.line_items[0].line_item_description);
+                            return;
+                        case 13:
+                            alert("Regional restrictions: " + data.purchase_receipt_info.line_items[0].line_item_description);
+                            return;
+                        case 24:
+                            alert("Missing base game: " + data.purchase_receipt_info.line_items[0].line_item_description);
+                            return;
+                        case 36:
+                            alert("Need a PS3?");
+                            return;
+                        case 50:
+                            alert("This is the recharge code!");
+                            return;
+                        default:
+                            alert("purchase_result_details: " + data.purchase_result_details);
+                            break;
+                    }
+                })
+                .catch(error => {
+                    // if (!data) return;
+                    console.error("Failed to parse data from response's JSON: " + error);
+                    throw error;
+                });
         });
     } else {
-        alert("ERROR! Not a valid Steam key: " + _key);
+        alert("ERROR! Not a valid Steam key.");
     }
 }
 
@@ -81,5 +132,3 @@ chrome.contextMenus.onClicked.addListener(function (info) {
         activatekey(info);
     }
 });
-
-// End of file;
